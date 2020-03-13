bf7eeb9b3dea369403d2111b53c925a065800ec7
unknown <sunjaelee83@gmail.com>
2020-03-13 16:01:10 +0000

    data attach

--- R/data.R
+++ R/data.R
@@ -1,14 +1,42 @@
-#' Prices of 50,000 round cut diamonds.
+#' KO-MSP mapping matrix
+#'
+#' A dataset containing KO mapping to gut MSP
+#'
+#' @format matrix
+#' \describe{
+#'   \item{row}{MSPs}
+#'   \item{column}{KEGG orthology terms}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"mspKoMat"
+
+
+
+#' KEGG orthology description table
 #'
 #' A dataset containing the prices and other attributes of almost 54,000
 #' diamonds.
 #'
 #' @format matrix
 #' \describe{
-#'   \item{mspKoMat}{example KO-MSP mapping matrix}
-#'   \item{koDescMap}{KO description}
-#'   \item{gutTaxo}{taxonomy of MSPs}
+#'   \item{KO}{KO ID}
+#'   \item{gene}{Gene ID}
+#'   \item{desc}{KO description}
+#' }
+#' @source \url{http://www.diamondse.info/}
+"koDescMap"
+
+
+#' MSP taxonomy table
+#'
+#' A dataset containing the taxonomy of MSPs
+#'
+#' @format data frame with taxonomy
+#' \describe{
+#'   \item{MSP}{MSP id}
+#'   \item{size}{the number of genes included}
+#'   \item{species}{species-level}
 #'   ...
 #' }
 #' @source \url{http://www.diamondse.info/}
-"MspData"
+"gutTaxo"
