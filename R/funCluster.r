4032db29286b5dacf76a93128e8e190c4e657291
unknown <sunjaelee83@gmail.com>
2020-03-13 16:05:51 +0000

    addd

--- R/funCluster.r
+++ R/funCluster.r
@@ -217,4 +217,7 @@ getCoveredMspByModules <- function(mspKoModules, mspKoMat, cutRatio=0.75, debug=
 #use_data(mspKoMat, koDescMap, gutTaxoTab, internal=T, overwrite = T)
 #system.file("")
 
+save(koDescMap, file="./data/koDescMap.RData")
+save(gutTaxoTab, file="./data/gutTaxoTab.RData")
+save(mspKoMat, file="./data/mspKoMat.RData")
 
